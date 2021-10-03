986e4d78ccbbabfb74794d22949b4f2a76d265a7
c0ndi <kowalik0konrad@gmail.com>
2021-10-04 00:17:34 +0200

    Initial coomit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Navbar />
+</template>
+<script setup>
+
+import Navbar from './components/Navbar.vue'
+import FirstSection from './components/FirstSection.vue'
+import SecondSection from './components/SecondSection.vue'
+import Footer from './components/Footer.vue'
+
+</script>
+
+
+<style lang="sass">
+* 
+  font-family: 'Poppins'
+  box-sizing: border-box
+
+body
+  margin: 0
+  padding: 0
+</style>
